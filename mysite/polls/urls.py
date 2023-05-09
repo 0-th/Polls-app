c304fc35f2c150d904de7ad4c7903ed8bd50c48e
0-th <artemisfowlx@protonmail.com>
2023-05-09 05:23:15 +0100

    fix bug with trailing slash in urls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path(route='', view=views.index, name='index'),
-    path(route='<int:question_id>', view=views.detail, name='detail'),
-    path(route='<int:question_id>/results', view=views.results, name='results'),
-    path(route='<int:question_id>/vote', view=views.vote, name='vote'),
+    path(route='<int:question_id>/', view=views.detail, name='detail'),
+    path(route='<int:question_id>/results/', view=views.results, name='results'),
+    path(route='<int:question_id>/vote/', view=views.vote, name='vote'),
 ]
