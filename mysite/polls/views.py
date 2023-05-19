c72268d5d02ee1606bde5f7e61f067fd724dc7b0
0-th <artemisfowlx@protonmail.com>
2023-05-19 23:09:05 +0100

    change some views to class-based views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,36 +1,27 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question, Choice
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(
-        request=request,
-        template_name='polls/index.html',
-        context=context
-    )
+from .models import Choice, Question
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(
-        request=request,
-        template_name='polls/detail.html',
-        context={'question': question}
-    )
+class IndexView(generic.ListView):
+    context_object_name = 'latest_question_list'
+    template_name = 'polls/index.html'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(klass=Question, pk=question_id)
-    return render(
-        request=request,
-        template_name='polls/results.html',
-        context={'question': question},
-    )
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
